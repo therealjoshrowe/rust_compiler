ef9544ab422328d924b4cb7eec533b94b50ca8d9
Joshua Rowe <jkrowe@ius.edu>
2016-05-31 23:14:40 -0400

    Continued adding support for keywords by finishing up the keyword list and adding function to load keywords into a dictionary opon execution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use std::collections::HashSet;
 use std::env;
 use std::io::Read;
 use std::error::Error;
-use std::io;
 use std::fs::File;
 
 const LEFT_PAREN: i32 = 101;
@@ -42,6 +42,8 @@ fn main() {
     let mut next_char;
     let mut token = 0;
     let mut lexeme = String::new();
+    let mut keywords: HashSet<String> = HashSet::new();
+    load_keywords(&mut keywords);
     //need to do bounds checking on args vec!
 
     let args: Vec<_> = env::args().collect();
@@ -67,7 +69,12 @@ fn main() {
                 lexeme.push(next_char);
                 next_char = read_char(&f);
             }
-            token = IDENT; // only handle identifiers currently
+            if keywords.contains(&lexeme) {
+                token = KEYWORD;
+            }
+            else {
+                token = IDENT;
+            }
         }
        else  if next_char.is_numeric() {
             while next_char.is_numeric() {
@@ -297,4 +304,54 @@ fn lookup(next_char: &mut char, f: &File) -> (i32, String) {
 }
 fn lex_error() -> ! {
     panic!("There was a lexical error");
+}
+fn load_keywords(s: &mut HashSet<String>) {
+    s.insert(String::from("abstract"));
+    s.insert(String::from("alignof"));
+    s.insert(String::from("as"));
+    s.insert(String::from("become"));
+    s.insert(String::from("box"));
+    s.insert(String::from("const"));
+    s.insert(String::from("continue"));
+    s.insert(String::from("crate"));
+    s.insert(String::from("do"));
+    s.insert(String::from("else"));
+    s.insert(String::from("enum"));
+    s.insert(String::from("extern"));
+    s.insert(String::from("false"));
+    s.insert(String::from("final"));
+    s.insert(String::from("fn"));
+    s.insert(String::from("for"));
+    s.insert(String::from("if"));
+    s.insert(String::from("impl"));
+    s.insert(String::from("in"));
+    s.insert(String::from("let"));
+    s.insert(String::from("loop"));
+    s.insert(String::from("macro"));
+    s.insert(String::from("match"));
+    s.insert(String::from("mod"));
+    s.insert(String::from("move"));
+    s.insert(String::from("offsetof"));
+    s.insert(String::from("override"));
+    s.insert(String::from("priv"));
+    s.insert(String::from("proc"));
+    s.insert(String::from("pub"));
+    s.insert(String::from("pure"));
+    s.insert(String::from("ref"));
+    s.insert(String::from("return"));
+    s.insert(String::from("Self"));
+    s.insert(String::from("Self"));
+    s.insert(String::from("sizeof"));
+    s.insert(String::from("struct"));
+    s.insert(String::from("super"));
+    s.insert(String::from("trait"));
+    s.insert(String::from("true"));
+    s.insert(String::from("type"));
+    s.insert(String::from("typeof"));
+    s.insert(String::from("unsafe"));
+    s.insert(String::from("unsized"));
+    s.insert(String::from("virtual"));
+    s.insert(String::from("where"));
+    s.insert(String::from("while"));
+    s.insert(String::from("yeild"));
 }
